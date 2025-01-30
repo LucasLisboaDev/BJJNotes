f37b33e6201c5a681b92dae5fd0691c5b63ceda8
Lucas Lisboa <lucaslisboalves@hotmail.com>
2025-01-30 15:18:40 -0500

    Initial Commit

---
+++ BJJNotes/BJJNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BJJNotesApp.swift
+//  BJJNotes
+//
+//  Created by Lucas Lisboa  on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BJJNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
